43e5e1f4591ed76f7e71d8412f6cc718da1090c3
git-me-in <egorgodzoev@gmail.com>
2025-12-19 22:02:18 +0700

    some testing

--- pack10/pack10task2.c
+++ pack10/pack10task2.c
@@ -1,4 +1,3 @@
-// E. Godzoev                                                                                                                                                   <- SuperMegaCoolGuy
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
