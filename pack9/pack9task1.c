9274454ac880d09bda3a8c6d4cebcfb3cfe94913
git-me-in <egorgodzoev@gmail.com>
2025-12-23 11:45:27 +0700

    9.1 finally

--- pack9/pack9task1.c
+++ pack9/pack9task1.c
@@ -3,91 +3,43 @@
 #include <string.h>
 #define LEN 8
 
-typedef struct Node
-{
-    int num;
-    char *data;
-    struct Node *next;
-} Node;
+typedef struct{
+    long long ind;
+    char data[LEN];
+}Stng;
 
-typedef struct List
-{
-    Node *current;
-    struct List *next;
-} List;
+void sort(Stng a[], int N){
+    for(int i = 1; i < N; i++){
+        Stng tmp = a[i];
+        int j = i - 1;
 
-Node *createNode(char *data, int num)
-{
-    Node *node = malloc(sizeof(Node));
-    node->data = malloc(LEN);
-    strcpy(node->data, data);
-    node->num = num;
-    node->next = NULL;
-    return node;
-}
-
-void pushNode(Node **head, char *data, int num)
-{
-    Node *node = createNode(data, num);
-
-    if (*head == NULL)
-    {
-        *head = node;
-        return;
-    }
-
-    Node *temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    temp->next = node;
-}
-
-void printList(List *head)
-{
-    while (head != NULL)
-    {
-        Node *temp = head->current;
-        while (temp != NULL)
+        while (j >= 0 && a[j].ind > tmp.ind)
         {
-            printf("%d %s\n", temp->num, temp->data);
-            temp = temp->next;
+            a[j + 1] = a[j];
+            j--;
         }
-        head = head->next;
+        a[j + 1] = tmp;
     }
 }
 
-int main(void)
-{
+int main(void){
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
     int N;
     scanf("%d", &N);
 
-    List *list = calloc(N, sizeof(List));
+    Stng arr[10000];
 
-    int ind;
-    char val[8];
-    for (int i = 0; i < N; i++)
-    {
-        scanf("%d %s", &ind, val);
-        pushNode(&list[ind].current, val, ind);
+    for(int i = 0; i < N; i++){
+        scanf("%lld %s", &arr[i].ind, arr[i].data);
     }
 
-    for (int i = 0; i < N; i++)
-    {
-        if (list[i].current != NULL)
-        {
-            while (list[i].current != NULL)
-            {
-                printf("%d %s\n", list[i].current->num, list[i].current->data);
-                list[i].current = list[i].current->next;
-            }
-        }
-    }
+    sort(arr, N);
 
-    printList(list);
+    for(int i = 0; i < N; i++){
+        printf("%lld %s\n", arr[i].ind, arr[i].data);
+    }
 
     fclose(stdin);
     fclose(stdout);
